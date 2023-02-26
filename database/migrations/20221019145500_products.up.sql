32298bd815dfc7051b90b06df2601d195f6297ec
VseZnal <Vseznal66@gmail.com>
2023-02-26 18:11:34 +0300

    chore: sql

--- database/migrations/20221019145500_products.up.sql
+++ database/migrations/20221019145500_products.up.sql
@@ -1,26 +1,5 @@
 BEGIN;
 
-CREATE OR REPLACE FUNCTION pseudo_encrypt(value BIGINT) returns BIGINT AS $$
-DECLARE
-    l1  BIGINT;
-    l2  BIGINT;
-    r1  BIGINT;
-    r2  BIGINT;
-    i   BIGINT := 0;
-BEGIN
-    l1:= (value >> 16) & 65535;
-    r1:= value & 65535;
-    WHILE i < 3 LOOP
-            l2 := r1;
-            r2 := l1 # ((((1366 * r1 + 150889) % 714025) / 714025.0) * 32767)::BIGINT;
-            l1 := l2;
-            r1 := r2;
-            i := i + 1;
-        END LOOP;
-    return ((r1 << 16) + l1);
-END;
-$$ LANGUAGE plpgsql strict immutable;
-
 CREATE SEQUENCE products_id_seq;
 
 CREATE TABLE products (
@@ -28,6 +7,7 @@ CREATE TABLE products (
     name         TEXT NOT NULL,
     size         TEXT NOT NULL,
     quantity     INTEGER,
+    in_reserve   INTEGER DEFAULT 0,
     warehouse_id INTEGER NOT NULL REFERENCES warehouse(id)
 );
 
